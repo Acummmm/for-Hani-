735d9a98de2f0e4ac6b405ca8154d96dcc0c3bed
Acummmm <138105369+Acummmm@users.noreply.github.com>
2026-07-26 13:34:42 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,15 @@
+/**
+ * ==========================================
+ * First Date Hani
+ * script.js
+ * ==========================================
+ */
+
+"use strict";
+
+/**
+ * Pindah ke slide berikutnya
+ */
 function nextSlide() {
-    alert("Slide 2 masih dalam proses 😆");
+    alert("💕 Slide 2 masih dalam proses... Tunggu update berikutnya ya 😆");
 }
